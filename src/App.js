8aada7b660cbeac144c647a6fc92356a461eb3d1
Christopher Quinn <caquinn.86@gmail.com>
2024-01-05 08:42:31 -0800

    Set up initial game state and added context wrapper to return statement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import {GameContext} from './GameContext'
+
 function App() {
+  const initState = {
+    xDim: 10,
+    yDim: 10,
+    totalMines: 10,
+    display: ["unset"],
+    dead: false,
+    win: false,
+    flagging: false
+  }
+
+  const [game, setGame] = useState(initState)
+
   return (
-    <div className="App">
-    </div>
+    <GameContext.Provider value={{game, setGame}}>
+      <div className="App">
+      </div>
+    </GameContext.Provider>
   );
 }
 
